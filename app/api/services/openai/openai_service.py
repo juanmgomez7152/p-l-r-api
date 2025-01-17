e5ddedff72d397dba6c2f33f13badd18a64a4fe8
Juan Gomez Guzman <104452048+juanmgomez7152@users.noreply.github.com>
2025-01-16 18:53:34 -0600

    refactor: Move OpenAI connection logic into OpenAiSession and update method calls for improved structure

--- app/api/services/openai/openai_service.py
+++ app/api/services/openai/openai_service.py
@@ -1,12 +1,16 @@
-from app.api.services.openai.open_ai_conn import openai_call, stream_openai_call
 from langdetect import detect
 from cachetools import TTLCache
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
 import logging
 
 logger = logging.getLogger(__name__)
 
-# with open("app/api/translation/translation_system_message.txt") as f:
-#     system_message = f.read()
+load_dotenv()
+model_name = "gpt-4o"
+client = OpenAI()
+client.api_key = os.getenv("OPENAI_API_KEY")
     
 class OpenAiSession:
     cache = TTLCache(maxsize=100, ttl=300)
@@ -20,6 +24,33 @@ class OpenAiSession:
         # self.history = [{"role": "system", "content": system_message}]
         return "System message set."
     
+    async def _openai_call(self, messages, model_name=model_name):
+        try:
+            response = client.chat.completions.create(
+                model=model_name,
+                messages=messages,
+                temperature=0
+            )
+    
+            answer = response.choices[0].message.content
+            return answer
+        except Exception as e:
+            logger.error("Error making connection to OpenAI: ", e)
+    async def _stream_openai_call(self, messages, model_name=model_name):
+        try:
+            response = client.chat.completions.create(
+            model=model_name,
+            messages=messages,
+            temperature=0,
+            stream=True
+            )
+        
+            for chunk in response:
+                yield chunk.choices[0].delta.content
+    
+        except Exception as e:
+            logger.error("Error making connection to OpenAI: ", e)
+    
     async def send_message(self, message):
         if message in self.cache:
             return self.cache[message]
@@ -31,18 +62,19 @@ class OpenAiSession:
             if language_detected == "es":
                 answer =  "El mensaje ya está en español, no es necesario traducirlo."
             else:
-                answer = await openai_call(payload)
+                answer = await self._openai_call(payload)
                 
             self.cache[message] = answer
             return answer
         except Exception as e:
             logger.error(f"Error detecting language: {e}")
             return "Lenguaje no detectado."
+        
     async def stream_message(self,message):
             payload = [{"role": "system", "content": self.system_message},
                         {"role": "user", "content": message}]
             try:
-                return stream_openai_call(payload)
+                return self.stream_openai_call(payload)
             except Exception as e:
                 logger.error(f"STREAM: Error detecting language: {e}")
                 return "Lenguaje no detectado."
