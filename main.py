7256292984fa21bb0835e175890f0fe11b30af7b
Juan Gomez Guzman <104452048+juanmgomez7152@users.noreply.github.com>
2025-02-13 17:31:25 -0600

    feat: Update CORS middleware to allow specific frontend URLs for improved security

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ app.include_router(api.router, prefix="/rest/api")
 # Add CORS middleware configuration
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=["https://p-l-r-ui-service-562328781960.us-east1.run.app","https://paramiguel.org"], # Update with your frontend URL
-    allow_origins=["*"],
+    allow_origins=["https://p-l-r-ui-service-562328781960.us-east1.run.app","https://paramiguel.org","http://localhost:4200/"], # Update with your frontend URL
+    # allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
